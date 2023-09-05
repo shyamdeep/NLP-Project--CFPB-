c16ae8a3aaa076bc4a91666e8b4b283134f53a87
Shyamdeep <79351912+shyamdeep@users.noreply.github.com>
2023-09-05 21:20:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,6 @@
 
 import joblib
 import re
-from sklearn.neural_network import MLPClassifier
-from sklearn.feature_extraction.text import TfidfVectorizer
 import streamlit as st
 from lime.lime_text import LimeTextExplainer
 import streamlit.components.v1 as components
@@ -68,26 +66,6 @@ if complaint_text != '':
             exp = explainer.explain_instance(complaint_text,model.predict_proba, num_features=10,top_labels=2)
             components.html(exp.as_html(), height=3500)
 
-
-# In[7]:
-
-
-#complaint_text = "applied property order able view several property order able use make consumer credit transaction purchase home filing report agency back think pertaining agency bank america recieved respone letter call time saying america mortgage never responded asociate ask come back bofa reapply went pa spoke asociate name show response letter showing previously said letter mean thing bank often send letter like customer said could assist put touch mortgage department said could assist said score low bank assist getting told federal law deny credit told denying simply stating bofa policy refuse give completed mortgage application anyway spoke phone told credit score low bofa policy unable assist extension consumer credit"
-
-
-# In[12]:
-
-
-#model.predict([complaint_text])[0]
-
-
-# In[15]:
-
-
-#model.predict_proba([complaint_text])[0][2]
-
-
-# In[ ]:
 
 
 
